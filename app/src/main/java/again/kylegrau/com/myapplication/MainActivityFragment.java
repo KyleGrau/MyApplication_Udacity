0a0b7691143e651e8a553bf6a4788840432a8afe
Kyle Grau <kylegrau@gmail.com>
2016-02-21 17:51:14 -0600

    Added menu and menu option "Refresh" to MainActivityFragment.

--- app/src/main/java/again/kylegrau/com/myapplication/MainActivityFragment.java
+++ app/src/main/java/again/kylegrau/com/myapplication/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -26,12 +28,17 @@ import java.util.List;
 public class MainActivityFragment extends Fragment {
 
     public MainActivityFragment() {
+
+    }
+
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forcastfragment, menu);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
+        this.setHasOptionsMenu(true);
         View rootView = inflater.inflate(R.layout.fragment_main,container, false);
         String[] data = {
                 "Mon 6/23 - Sunny - 31/17",
