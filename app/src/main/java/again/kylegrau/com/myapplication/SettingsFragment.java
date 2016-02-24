b43d81902b5a1915b2b583d7a273ca973e3aeed5
Kyle Grau <kylegrau@gmail.com>
2016-02-23 19:26:21 -0600

    Summary of preference now updates when changed for Location.

--- app/src/main/java/again/kylegrau/com/myapplication/SettingsFragment.java
+++ app/src/main/java/again/kylegrau/com/myapplication/SettingsFragment.java
@@ -1,16 +1,50 @@
 package again.kylegrau.com.myapplication;
 
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 /***
  * Created by kgrau on 2/23/2016.
  */
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_general);
+        
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.location_key)));
+    }
+
+    //Private method to implement binding summary to value
+    private void bindPreferenceSummaryToValue(Preference preference) {
+        preference.setOnPreferenceChangeListener(this);
+
+        //Trigger immediately to seed default value
+        onPreferenceChange(preference, PreferenceManager.getDefaultSharedPreferences(preference.getContext()).getString(preference.getKey(), ""));
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String stringValue = newValue.toString();
+
+        if(preference instanceof ListPreference) {
+            //Find key value pair for list preferences
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue(stringValue);
+            if (prefIndex >= 0) {
+                preference.setSummary(stringValue);
+            }
+        }   else {
+            //Set summary to user preference
+            preference.setSummary(stringValue);
+
+        }
+
+        return true;
     }
 }
