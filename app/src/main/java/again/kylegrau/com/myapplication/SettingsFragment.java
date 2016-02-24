d010d998b54f88707cfead86b3e3e70053dcd1ec
Kyle Grau <kylegrau@gmail.com>
2016-02-23 18:12:24 -0600

    Added settings.

---
+++ app/src/main/java/again/kylegrau/com/myapplication/SettingsFragment.java
@@ -0,0 +1,16 @@
+package again.kylegrau.com.myapplication;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by kgrau on 2/23/2016.
+ */
+public class SettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref_general);
+    }
+}
